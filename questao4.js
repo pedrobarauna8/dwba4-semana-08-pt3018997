5f5ff0af15ddc4c05ab5d3bb57ca1c20b207a592
pedrobarauna8 <81633121+pedrobarauna8@users.noreply.github.com>
2023-09-21 23:48:17 +0000

    sim

--- questao4.js
+++ questao4.js
@@ -16,7 +16,7 @@ media(){
 }
 
 situacao(){
-  if (this.media>6){
+  if ((this.primeiraNota*0.6)+(this.segundaNota*0.4)>6){
     alert("Aprovado")
   }else{
     alert("Reprovado")
@@ -25,10 +25,17 @@ situacao(){
   
 }
 
-var aluno = new Aluno("Matheus", "Silva", 8, 9)
-aluno.nomeCompleto()
-aluno.media()
-aluno.situacao()
+var aluno1 = new Aluno("Matheus", "Silva", 8, 9)
+aluno1.nomeCompleto()
+aluno1.media()
+aluno1.situacao()
 
+var aluno2 = new Aluno("Cleber", "Pirone", 0, 1)
+aluno2.nomeCompleto()
+aluno2.media()
+aluno2.situacao()
 
-
+var aluno3 = new Aluno("Gustavo", "Lopes", 10, 8)
+aluno3.nomeCompleto()
+aluno3.media()
+aluno3.situacao()
